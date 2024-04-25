2a389b9a9468e5862a51b480e7476e1ec000fb41
wwoch <witekwoch@gmail.com>
2024-04-26 00:40:49 +0200

    add robot spawning in gazebo environment

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 To launch the robot visualization in RViz, you need to run the `run_exp_bot.sh` script. This script sets up the necessary environment and configurations for visualizing the robot model in RViz, a 3D visualization tool for ROS (Robot Operating System).
 
-## How to Run
-
 Follow these steps to start the visualization:
 
 1. Open a terminal.
@@ -16,5 +14,21 @@ Follow these steps to start the visualization:
    ```bash
    bash run_exp_bot.sh
    ```
-   
+
+# Spawning the Robot in Gazebo
+
+To launch the robot in the Gazebo simulator, you need to execute the `run_gazebo_env.sh` script. This script prepares the required environment and settings for simulating the robot model in Gazebo, a dynamic simulation software for robotics applications.
+
+Follow these instructions to start the simulation:
+
+1. Open a terminal.
+2. Navigate to the directory containing the `run_gazebo_env.sh` script.
+3. Ensure the script is executable. If it is not, you can make it executable by running:
+   ```bash
+   chmod +x run_gazebo_env.sh
+      ```
+4. Run the Script: Execute the script by running:
+   ```bash
+   bash run_gazebo_env.sh
+   ```
 
---
+++ src/simulation_environment/launch/env_gazebo.launch.py
@@ -0,0 +1,49 @@
+import os
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_path, get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch_ros.actions import Node
+
+def generate_launch_description():
+ 
+    urdf_path = os.path.join(get_package_share_path('explorer_bot'),
+                             'urdf', 'my_robot.urdf.xacro')
+
+    gazebo_world_path = os.path.join(get_package_share_path('simulation_environment'),
+                             'worlds', 'sq_wall.world')
+
+
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    )
+
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}],
+        output='screen'
+    )
+
+
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
+        output='screen'
+    )
+
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher,
+        spawn_entity,
+    ])
+
