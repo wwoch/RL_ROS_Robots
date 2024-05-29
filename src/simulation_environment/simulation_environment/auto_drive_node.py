f5a335008fc8ee8de20916f62fab0cf1dc8b86c1
wwoch <witekwoch@gmail.com>
2024-05-29 15:54:02 +0200

    fix json file to save only important data

--- src/simulation_environment/simulation_environment/auto_drive_node.py
+++ src/simulation_environment/simulation_environment/auto_drive_node.py
@@ -16,20 +16,21 @@ class AutoDriveNode(Node):
         self.subscription_distance = self.create_subscription(Float32, '/distance_to_obstacle', self.distance_callback, 10)
         self.subscription_collision = self.create_subscription(Bool, '/collision_detected', self.obstacle_callback, 10)
 
-        self.data_timer = self.create_timer(0.1, self.data_timer_callback)  # Co ile odbierać dane
+        self.data_timer = self.create_timer(0.2, self.data_timer_callback)  # Co ile odbierać dane
         self.direction_timer = self.create_timer(1.0, self.direction_timer_callback)  # Co ile zmieniać kierunek jazdy
         self.time_check_timer = self.create_timer(0.1, self.time_check_callback)  # Timer do czasu symulacji
 
         self.current_direction = random.uniform(-(math.pi/2), math.pi/2)
         self.current_velocity = 0.5
         self.distance_to_obstacle = None
+        self.previous_distance = None
 
         self.reset_simulation_client = self.create_client(Empty, '/reset_simulation')
         self.simulation_count = 0
         self.simulation_limit = 10  
         self.simulation_start_time = self.get_clock().now()  
         self.simulation_duration = None  
-        self.simulation_time_limit = 10.0
+        self.simulation_time_limit = 5.0
         
         self.publish_velocity()
         self.clear_json_file()
@@ -148,29 +149,31 @@ class AutoDriveNode(Node):
         if self.distance_to_obstacle is not None:
             distance_str = self.distance_to_string(self.distance_to_obstacle)
         else:
-            distance_str = "unknown"
+            distance_str = "far"
         
         if self.current_direction is not None:
             direction_str = self.direction_to_string(self.current_direction)
         else:
             direction_str = "unknown"
             
-        data = {
-            "simulation_count": self.simulation_count,
-            "distance_to_obstacle": distance_str,
-            "current_direction": direction_str,
-            "simulation_duration": duration_in_seconds
-        }
-        try:
-            with open('driving_data.json', 'r') as json_file:
-                data_history = json.load(json_file)
-        except FileNotFoundError:
-            data_history = []
-
-        data_history.append(data)
-
-        with open('driving_data.json', 'w') as json_file:
-            json.dump(data_history, json_file, indent=4)
+        if self.previous_distance != distance_str:
+            self.previous_distance = distance_str
+            data = {
+                "simulation_count": self.simulation_count,
+                "distance_to_obstacle": distance_str,
+                "current_direction": direction_str,
+                "simulation_duration": duration_in_seconds
+            }
+            try:
+                with open('driving_data.json', 'r') as json_file:
+                    data_history = json.load(json_file)
+            except FileNotFoundError:
+                data_history = []
+    
+            data_history.append(data)
+    
+            with open('driving_data.json', 'w') as json_file:
+                json.dump(data_history, json_file, indent=4)
 
 def main(args=None):
     rclpy.init(args=args)
